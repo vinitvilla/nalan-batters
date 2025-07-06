0938d9753bfadcb2a54fea97a4cdf530c19f0663
Vinith babu <vinithonboard@gmail.com>
2025-07-06 19:32:26 -0400

    Initializing the repository

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'picsum.photos',
+    },
+    {
+      protocol: 'https',
+      hostname: 'firebasestorage.googleapis.com',
+      port: '',
+      pathname: '/**',
+    },
+  ],
+},
 };
 
 export default nextConfig;
